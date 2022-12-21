b5a44d1924a225893598cd4ad5376404c786fa76
Ubuntu_16.04 <simon.huebers@fh-bielefeld.de>
2022-12-21 17:14:26 +0100

    added messages to Server

--- client.C
+++ client.C
@@ -11,6 +11,7 @@
 #include <sstream>
 
 #include "SIMPLESOCKET.H"
+#include "TASK1.H"
 
 using namespace std;
 
@@ -19,25 +20,57 @@ int main() {
 	TCPclient c;
 	string host = "localhost";
 	string msg;
-	stringstream ss;
-
+	stringstream clientRequest;
+	string SYMBOLS = "ABCDEFGHIJKLMNOPQRTSTUVWXYZabcdefghijklmopqrstuvwxyz0123456789";
+	int pwdLength = 4;
+	int symbSetSize = 5;
 	//connect to host
 	c.conn(host , 2022);
 
-	int pwdLength;
-	int symbSetSize;
-
-
-	ss << "GENERATE[" << pwdLength << "," << symbSetSize << "]";
-	msg = ss.str;
-
+	msg = "Client bereit";
 	cout << "client sends:" << msg << endl;
 	c.sendData(msg);
-	msg = c.receive(32);
+	msg = c.receive(128);
 	cout << "got response:" << msg << endl;
-	//sleep(1);
+	sleep(1);
+
+	for(int i=0; i<4; i++){
+
+		if(msg == "Server bereit"){
+
+			clientRequest << "GENERATE[" << pwdLength << "," << symbSetSize << "]";
+			msg = clientRequest.str();
+
+			cout << "client sends:" << msg << endl;
+			c.sendData(msg);
+			msg = c.receive(128);
+			cout << "got response:" << msg << endl;
+			sleep(1);
+		}
+		else if(msg.compare(0,18,"Passwort generiert") == 0){
+
+			msg = "noch nicht implementiert";
+
+			cout << "client sends:" << msg << endl;
+			c.sendData(msg);
+			msg = c.receive(128);
+			cout << "got response:" << msg << endl;
+			sleep(1);
+		}
+		else{
+			msg = "Error";
+
+			cout << "client sends:" << msg << endl;
+			c.sendData(msg);
+			msg = c.receive(128);
+			cout << "got response:" << msg << endl;
+			sleep(1);
+		}
+
+	}
+
+
 	c.sendData("BYEBYE");
-	msg = c.receive(32);
 	cout << "got response:" << msg << endl;
 
 
